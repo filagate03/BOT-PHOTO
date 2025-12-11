8bdc20f7831805e66e30851d20e8d6e6b0ab51aa
filagate03 <alexfarley555@gmail.com>
2025-12-12 00:13:22 +0300

    add src/bot_photo/models/__init__.py

--- src/bot_photo/models/__init__.py
+++ src/bot_photo/models/__init__.py
@@ -1,6 +1,7 @@
 from .face import Face
 from .prompt_generation import PromptGeneration
 from .session import Session
+from .payment import Payment
 from .states import AdminState, AgreementState, PhotoSessionState, PromptState
 from .user import User
 
@@ -8,6 +9,7 @@ __all__ = [
     "Face",
     "PromptGeneration",
     "Session",
+    "Payment",
     "AdminState",
     "AgreementState",
     "PhotoSessionState",
